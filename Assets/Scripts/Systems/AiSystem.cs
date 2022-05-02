772cb02a1f34eb08dff7bee457c02fe5770640ce
AlexWargon <wargonger@gmail.com>
2022-05-02 14:12:29 +0300

    AI System error fix

--- Assets/Scripts/Systems/AiSystem.cs
+++ Assets/Scripts/Systems/AiSystem.cs
@@ -5,16 +5,15 @@ using UnityEngine;
 namespace TicTacToeEcs {
     public partial class AiSystem : SystemBase {
         private EntityQuery _freeCells;
-        private EntityQuery _gameOverFlag;
+
         protected override void OnCreate() {
             _freeCells = GetEntityQuery(typeof(Cell), ComponentType.Exclude<Sign>());
-            _gameOverFlag = GetEntityQuery(typeof(GameOverFlag));
         }
 
         protected override void OnUpdate() {
             
             Entities.WithAny<AiTurnFlag>().ForEach((in Entity turnEntity) => {
-                if(!_gameOverFlag.IsEmptyIgnoreFilter) return;
+                if(_freeCells.IsEmpty) return;
                 var freeCellEntities = _freeCells.ToEntityArray(Allocator.Temp);
 
                 var target = Random.Range(0, freeCellEntities.Length);
